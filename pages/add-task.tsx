5bda02d23555a63086cf12451aab33e913a50574
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-04 13:24:55 +0530

    fix: antd error occured and fixed it

---
+++ pages/add-task.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd"
+import React from "react"
+
+const AddTask = () => {
+    return (
+        <div>
+            <div>AddTask</div>
+            <Button type="primary">Add task</Button>
+        </div>
+    )
+}
+
+export default AddTask
