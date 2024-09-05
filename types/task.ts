136f9376fe526b5b19b3ee12b66cc8537e6da908
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-05 16:29:42 +0530

    fix: change the type of priority

--- types/task.ts
+++ types/task.ts
@@ -1,9 +1,7 @@
-import { MenuProps } from "antd"
-
 export interface Task {
     key: React.Key
     task: string
     status: "pending" | "completed"
     dueDate: Date | null
-    // priority: MenuProps["items"]
+    priority: string
 }
