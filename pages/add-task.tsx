5849417209670d1927f6ab385cf6fed7a111c384
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-05 14:46:44 +0530

    refactor: add task page

--- pages/add-task.tsx
+++ pages/add-task.tsx
@@ -1,11 +1,30 @@
-import { Button } from "antd"
+import Layout from "@/components/Layout"
+import { InfoCircleOutlined } from "@ant-design/icons"
+import {
+    Button,
+    DatePicker,
+    Flex,
+    Form,
+    Input,
+    Radio,
+    Select,
+    Space,
+    Typography
+} from "antd"
+import { useState } from "react"
+import { redirect } from "next/navigation"
+import TaskForm from "@/components/TaskForm"
+
+const { Title } = Typography
 
 const AddTask = () => {
     return (
-        <div>
-            <div>Add task</div>
-            <Button type="primary">Add task</Button>
-        </div>
+        <Layout>
+            <Flex vertical align="center" style={{ width: "100%" }}>
+                <Title level={4}>Create a Task</Title>
+                <TaskForm />
+            </Flex>
+        </Layout>
     )
 }
 
