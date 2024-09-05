9d0f529eaafe47bfdc46fc7ea34eba59d4f05fdf
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-05 16:30:18 +0530

    update: task list

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,43 +1,54 @@
-import {
-    Button,
-    Dropdown,
-    Popconfirm,
-    Space,
-    Table,
-    Tag,
-    Typography
-} from "antd"
+import { Button, Dropdown, Popconfirm, Space, Table, Typography } from "antd"
 import React, { useState } from "react"
 import { Task } from "@/types/task"
 import {
-    CheckCircleOutlined,
-    ClockCircleOutlined,
     DeleteOutlined,
     DownOutlined,
     QuestionCircleOutlined
 } from "@ant-design/icons"
+import { taskStatus } from "@/util/taskStatus"
+import { dateFormat } from "@/util/dateFormat"
+import { MenuProps } from "antd"
 
 const { Column } = Table
 const { Text } = Typography
 
 const TaskList: React.FC = () => {
+    const items: MenuProps["items"] = [
+        {
+            key: "1",
+            label: "Low"
+        },
+        {
+            key: "2",
+            label: "Medium"
+        },
+        {
+            key: "3",
+            label: "High"
+        }
+    ]
+
     const [taskList, setTaskList] = useState<Task[]>([
         {
             key: "1",
             task: "Complete the home works",
             status: "pending",
+            priority: "Low",
             dueDate: new Date("2024-09-03")
         },
         {
             key: "2",
             task: "Clean the room",
             status: "completed",
+            priority: "Low",
             dueDate: null
         },
         {
             key: "3",
             task: "Watch a new movie",
             status: "pending",
+            priority: "Low",
             dueDate: null
         }
     ])
@@ -47,89 +58,36 @@ const TaskList: React.FC = () => {
         setTaskList(newData)
     }
 
-    // function for -> set task status
-    const taskStatus = (status: string): JSX.Element => {
-        switch (status) {
-            case "pending":
-                return (
-                    <Tag
-                        color={"blue"}
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "flex-start",
-                            maxWidth: 120,
-                            gap: 4
-                        }}
-                    >
-                        <ClockCircleOutlined />
-                        <Text style={{ color: "blue" }}>Pending</Text>
-                    </Tag>
-                )
-            case "completed":
-                return (
-                    <Tag
-                        color={"green"}
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "flex-start",
-                            maxWidth: 120,
-                            gap: 4
-                        }}
-                    >
-                        <CheckCircleOutlined />
-                        <Text style={{ color: "green" }}>Completed</Text>
-                    </Tag>
-                )
-            default:
-                return (
-                    <Tag
-                        color={"blue"}
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "flex-start",
-                            maxWidth: 120,
-                            gap: 4
-                        }}
-                    >
-                        <ClockCircleOutlined />
-                        <Text style={{ color: "blue" }}>Pending</Text>
-                    </Tag>
-                )
-        }
-    }
-
     return (
         <Table dataSource={taskList} bordered>
             <Column title="Task" dataIndex="task" key="task" />
-
             <Column
                 title="Status"
                 dataIndex="status"
                 key="status"
                 render={(status: string) => taskStatus(status)}
             />
-
-            <Column title="Due Date" dataIndex="dueDate" key="dueDate" />
-
+            <Column
+                title="Due Date"
+                dataIndex="dueDate"
+                key="dueDate"
+                render={(dueDate: Date | null) => dateFormat(dueDate)}
+            />
             <Column
                 title="Priority"
                 dataIndex="priority"
                 key="priority"
                 render={() => (
-                    <Dropdown menu={{}} trigger={["click"]}>
+                    <Dropdown menu={{ items }} trigger={["click"]}>
                         <a onClick={(e) => e.preventDefault()}>
                             <Space>
-                                click
+                                Set Priority Level
                                 <DownOutlined />
                             </Space>
                         </a>
                     </Dropdown>
                 )}
             />
-
             <Column
                 title=""
                 key="action"
