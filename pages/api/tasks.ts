2f6934527b229e9e26517e123fd155503fab3cf9
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-06 14:49:56 +0530

    update: Create the task list array & update GET request with the task list

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -2,9 +2,33 @@
 import { Task } from "@/types/task"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-let tasks: Task[] = []
+let tasks: Task[] = [
+    {
+        key: "1",
+        task: "Complete the home works",
+        status: "pending",
+        priority: "low",
+        dueDate: new Date("2024-09-03")
+    },
+    {
+        key: "2",
+        task: "Clean the room",
+        status: "pending",
+        priority: "medium",
+        dueDate: null
+    },
+    {
+        key: "3",
+        task: "Watch a new movie",
+        status: "pending",
+        priority: "low",
+        dueDate: null
+    }
+]
 
 export default function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Task>
-) {}
+    res: NextApiResponse<Task[]>
+) {
+    res.status(200).json(tasks)
+}
