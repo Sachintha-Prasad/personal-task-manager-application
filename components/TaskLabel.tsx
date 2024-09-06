90dd6ce4262b7e6bdcc5f20a616a0f4bce8d4bd0
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-06 11:21:24 +0530

    update: task status util function changed to task label component

---
+++ components/TaskLabel.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { Tag, Typography } from "antd"
+import { CheckCircleOutlined, ClockCircleOutlined } from "@ant-design/icons"
+
+const { Text } = Typography
+
+type TaskLabelProp = { status: "pending" | "completed" }
+
+const tagStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    maxWidth: 120,
+    borderRadius: "120px",
+    gap: 4
+}
+
+const TaskLabel = ({ status }: TaskLabelProp) => {
+    if (status === "pending") {
+        return (
+            <Tag color={"blue"} icon={<ClockCircleOutlined />} style={tagStyle}>
+                <Text style={{ color: "blue" }}>Pending</Text>
+            </Tag>
+        )
+    } else if (status === "completed") {
+        return (
+            <Tag
+                color={"green"}
+                icon={<CheckCircleOutlined />}
+                style={tagStyle}
+            >
+                <Text style={{ color: "green" }}>Completed</Text>
+            </Tag>
+        )
+    }
+}
+
+export default TaskLabel
