b3b8f864fd9e2fa9b09b3fc0a8ecf674ab4191d8
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-06 11:24:44 +0530

    update: task api update

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Task } from "@/types/task"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-type Data = {
-    name: string
-}
+let tasks: Task[] = []
 
 export default function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
-) {
-    res.status(200).json({ name: "John Doe" })
-}
+    res: NextApiResponse<Task>
+) {}
