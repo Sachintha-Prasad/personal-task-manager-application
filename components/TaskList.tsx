fe7d90d24dfc57b948d2aa268c32a23ca84572f1
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-08 11:20:36 +0530

    update: update the tasks delete, status change, priority level change functionalities

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -2,6 +2,7 @@ import {
     Button,
     Checkbox,
     Empty,
+    message,
     Popconfirm,
     Select,
     Table,
@@ -9,48 +10,59 @@ import {
 } from "antd"
 import React, { useEffect, useState } from "react"
 import { Task } from "@/types/task"
-import {
-    DeleteOutlined,
-    PlusCircleOutlined,
-    QuestionCircleOutlined
-} from "@ant-design/icons"
+import { DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons"
 import { dateFormat } from "@/util/dateFormat"
 import PriorityLabel from "./PriorityLabel"
 import TaskLabel from "./TaskLabel"
-
-const { Column } = Table
-const { Text } = Typography
+import {
+    getTasksFromLocalStorage,
+    saveTaskToLocalStorage
+} from "@/util/localStorageFunctions"
 
 const TaskList = () => {
     const [taskList, setTaskList] = useState<Task[]>([])
 
-    const handleDelete = (key: React.Key) => {
-        const updatedTasks: Task[] = taskList.filter((item) => item.key !== key)
-        setTaskList(updatedTasks)
+    const handleTaskDelete = (key: React.Key) => {
+        const updatedTasksList: Task[] = taskList.filter(
+            (item) => item.key !== key
+        )
+        updatedTasksList.length > 0
+            ? message.success("Task deleted successfully!")
+            : message.info("Currently! There are no tasks.")
+        setTaskList(updatedTasksList)
+        saveTaskToLocalStorage(updatedTasksList)
     }
 
     const handleTaskStatusChange = (key: React.Key, checked: boolean) => {
-        const updatedTasks: Task[] = taskList.map((task) =>
+        const updatedTasksList: Task[] = taskList.map((task) =>
             task.key === key
                 ? { ...task, status: checked ? "completed" : "pending" }
                 : task
         )
-        setTaskList(updatedTasks)
+        message.success("Task status changed!")
+        setTaskList(updatedTasksList)
+        saveTaskToLocalStorage(updatedTasksList)
+    }
+
+    const handleTasksPriorityLevelChange = (
+        key: React.Key,
+        priority: "low" | "medium" | "high"
+    ) => {
+        const updatedTasksList: Task[] = taskList.map((task) =>
+            task.key === key ? { ...task, priority: priority } : task
+        )
+        message.success("Task's priority changed!")
+        setTaskList(updatedTasksList)
+        saveTaskToLocalStorage(updatedTasksList)
     }
 
     useEffect(() => {
-        const fetchTasks = async () => {
-            const response = await fetch("api/tasks")
-            const data = await response.json()
-            console.log(data)
-            setTaskList(data)
-        }
-
-        fetchTasks()
+        setTaskList(getTasksFromLocalStorage())
     }, [])
 
     return (
         <Table
+            dataSource={taskList}
             //locale is used to show things when the table is empty
             locale={{
                 emptyText: () => (
@@ -58,25 +70,17 @@ const TaskList = () => {
                         image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
                         imageStyle={{ height: 60 }}
                         description={
-                            <Text>
+                            <Typography.Text>
                                 Currently! There are no tasks. Please add new
                                 tasks.
-                            </Text>
+                            </Typography.Text>
                         }
-                    >
-                        <Button type="dashed">
-                            <PlusCircleOutlined />
-                            Add Task{" "}
-                        </Button>
-                    </Empty>
+                    />
                 )
             }}
-            // loading={{ delay: 3 }}
-            dataSource={taskList}
-            style={{ borderRadius: 24 }}
             bordered
         >
-            <Column
+            <Table.Column
                 title=""
                 width={40}
                 render={(record: Task) => (
@@ -88,10 +92,8 @@ const TaskList = () => {
                     />
                 )}
             />
-
-            <Column title="Task" dataIndex="task" key="task" />
-
-            <Column
+            <Table.Column title="Task" dataIndex="task" key="task" />
+            <Table.Column
                 title="Status"
                 dataIndex="status"
                 key="status"
@@ -100,26 +102,24 @@ const TaskList = () => {
                     <TaskLabel status={status} />
                 )}
             />
-
-            <Column
+            <Table.Column
                 title="Due Date"
                 dataIndex="dueDate"
                 key="dueDate"
                 width={160}
                 render={(dueDate: Date | null) => dateFormat(dueDate)}
             />
-
-            <Column
+            <Table.Column
                 title="Priority"
                 dataIndex="priority"
                 key="priority"
                 width={160}
-                render={(priority: "low" | "medium" | "high") => (
+                render={(priority: "low" | "medium" | "high", record: Task) => (
                     <Select
-                        defaultValue={{
-                            value: priority,
-                            label: <PriorityLabel type={priority} />
-                        }}
+                        defaultValue={priority}
+                        onChange={(priority) =>
+                            handleTasksPriorityLevelChange(record.key, priority)
+                        }
                         style={{
                             width: "100%",
                             maxWidth: 140
@@ -141,8 +141,7 @@ const TaskList = () => {
                     />
                 )}
             />
-
-            <Column
+            <Table.Column
                 title=""
                 key="action"
                 width={40}
@@ -155,7 +154,7 @@ const TaskList = () => {
                         }
                         okText="Yes"
                         cancelText="No"
-                        onConfirm={() => handleDelete(record.key)}
+                        onConfirm={() => handleTaskDelete(record.key)}
                     >
                         <Button
                             danger
